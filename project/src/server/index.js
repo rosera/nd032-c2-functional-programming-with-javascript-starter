be3fa7f4b490dac6db6be2c97310ebae52494c6c
rosera <askrichardrose@gmail.com>
2020-09-28 18:40:29 +0100

    Task2: Update port + ENV VAR

--- project/src/server/index.js
+++ project/src/server/index.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch')
 const path = require('path')
 
 const app = express()
-const port = 3000
+const port = 8080 || process.env.PORT
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
