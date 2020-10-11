10b05ef70cdd756f18c869263eb0f2aac147e421
rosera <askrichardrose@gmail.com>
2020-10-11 13:41:52 +0100

    Update: Revised UI with portfolio gallery for images

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -8,18 +8,17 @@ let store = {
 
 // add our markup to the page
 const root = document.getElementById('root')
-const curiosity = document.getElementById('curiosity')
-
-let slideIndex = 1;
-
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-  render(root, store)
-  renderRover(curiosity, store)
+  
+  // Perform Rover workflow
+  store.rovers.map((rover) => {
+    renderRover(rover, store);
+  });
 
-  // Enable the slideshow
-  showSlides(slideIndex);
+  render(root, store)
+
 })
 
 
@@ -64,10 +63,7 @@ const ImageOfTheDay = (apod) => {
     // If image does not already exist, or it is not from today -- request it again
     const today = new Date()
     const photodate = new Date(apod.date)
-    
-    console.log("IOTD Dates: " + photodate.getDate(), today.getDate());
 
-    console.log(photodate.getDate() === today.getDate());
     if (!apod || apod.date === today.getDate() ) {
         getImageOfTheDay(store)
     }
@@ -100,190 +96,88 @@ const getImageOfTheDay = (state) => {
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }))
 
-    return data
+    return apod 
 }
 
 
 
-// TODO: Temporary Function - will be replaced
-// ------------------------------------------------------  COMPONENTS
-function tempButtonFunction(){
-  renderRover(curiosity, store)
-}
 
-const updateStoreRover = (store, newState) => {
+const updateStoreRover = (rover, store, newState) => {
     store = Object.assign(store, newState)
-    renderRover(curiosity, store)
+    renderRover(rover, store)
 }
 
-const renderRover = async (root, state) => {
-    root.innerHTML = AppRover(state)
+const renderRover = async (rover, state) => {
+    const marsrover = document.getElementById(rover)
+    marsrover.innerHTML = AppRover(rover, state)
 }
 
-const AppRover = (state) => {
+const AppRover = (rover, state) => {
     let { rovers, apod, mars } = state
 
     return `
-      ${ImageOfTheDayRover(mars)}
+      ${ImageOfTheDayRover(rover, mars)}
     `
 }
 
-// Example of a pure function that renders information requested from the backend
-const ImageOfTheDayRoverTemp = (mars) => {
 
+// Example of a pure function that renders information requested from the backend
+const ImageOfTheDayRover = (rover, mars) => {
     // Check if the object exists
     if (!mars) {
-        getImageOfTheDayRover(store)
+        getImageOfTheDayRover(rover, mars)
     }
 
+    if (typeof(mars.response.photos) !== 'undefined') {
     // Create the html 
     return (`
-       <main>
        <section>
+			 <div id="${mars.response.photos[0].rover.name}">
 			 <div class="content">
 			   <div class="inner">
            <h1>Rover: ${mars.response.photos[0].rover.name}</h1>
-           <p>Curiosity is a car-sized rover designed to explore the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission.[2] <br>Curiosity was launched from Cape Canaveral on November 26, 2011, at 15:02 UTC and landed on Aeolis Palus inside Gale on Mars on August 6, 2012, 05:17 UTC.<br>[5][6][9] The Bradbury Landing site was less than 2.4 km (1.5 mi) from the center of the rover's touchdown target after a 560 million km (350 million mi) journey.[10][11] The rover's goals include an investigation of the Martian climate and geology; assessment of whether the selected field site inside Gale has ever offered environmental conditions favorable for microbial life, including investigation of the role of water; and planetary habitability studies in preparation for human exploration.
-           </p>
-           <a href="https://en.wikipedia.org/wiki/Curiosity_(rover)"Mars Rover Wikipedia excerpt"</a>
            <h3>Mission Information: ${mars.response.photos[0].rover.name}</h3>
-           Mars ID: ${mars.response.photos[0].id} | Mars launch date: ${mars.response.photos[0].rover.launch_date} | Mars landing date: ${mars.response.photos[0].rover.landing_date}<br>
-           Mars status: ${mars.response.photos[0].rover.status} | Mars image date: ${mars.response.photos[0].earth_date}<br></p>
-
-           <div class="slideshow-container">
-             <div class="mySlides fade">
-               <div class="numbertext">1 / 3</div>
-                  <img src="${mars.response.photos[0].img_src}" data-position="center center" style="width:100%">
-               <div class="text">${mars.response.photos[0].camera.full_name}:${mars.response.photos[0].id}</div>
-             </div>
-
-             <div class="mySlides fade">
-               <div class="numbertext">2 / 3</div>
-                  <img src="${mars.response.photos[1].img_src}" data-position="center center" style="width:100%">
-               <div class="text">${mars.response.photos[1].camera.full_name}:${mars.response.photos[1].id}</div>
-             </div>
-
-             <div class="mySlides fade">
-               <div class="numbertext">3 / 3</div>
-                  <img src="${mars.response.photos[2].img_src}" data-position="center center" style="width:100%">
-               <div class="text">${mars.response.photos[2].camera.full_name}:${mars.response.photos[2].id}</div>
-             </div>
-
-             <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-             <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-          </div>
-          <br>
-
-          <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span> 
-            <span class="dot" onclick="currentSlide(2)"></span> 
-            <span class="dot" onclick="currentSlide(3)"></span> 
-          </div>
+           <p> Identifier: ${mars.response.photos[0].rover.id} | Launch Date: ${mars.response.photos[0].rover.launch_date} | Landing Date: ${mars.response.photos[0].rover.landing_date} | Status: ${mars.response.photos[0].rover.status} | Image date: ${mars.response.photos[0].earth_date}</p>
+           <h3>Portfolio: ${mars.response.photos[0].rover.name}</h3>
+           <div class="row">
+              ${buildPortfolioGallery(mars.response.photos)}
+           </div>
 				</div>
-			</div>
-      </section>
-      </main>
-   `)
-}
-
-// Example of a pure function that renders information requested from the backend
-const ImageOfTheDayRover = (mars) => {
-
-    // Check if the object exists
-    if (!mars) {
-        getImageOfTheDayRover(store)
-    }
-
-    // Create the html 
-    return (`
-       <section>
-			 <div class="content">
-			   <div class="inner">
-           <h1>Rover: ${mars.response.photos[0].rover.name}</h1>
-           <a href="https://en.wikipedia.org/wiki/Curiosity_(rover)"><h3>Wikipedia Information: ${mars.response.photos[0].rover.name}</h3></a>
-           <p>Curiosity is a car-sized rover designed to explore the Gale crater on Mars as part of NASA's Mars Science Laboratory (MSL) mission.[2] <br>Curiosity was launched from Cape Canaveral on November 26, 2011, at 15:02 UTC and landed on Aeolis Palus inside Gale on Mars on August 6, 2012, 05:17 UTC.<br>[5][6][9] The Bradbury Landing site was less than 2.4 km (1.5 mi) from the center of the rover's touchdown target after a 560 million km (350 million mi) journey.[10][11] The rover's goals include an investigation of the Martian climate and geology; assessment of whether the selected field site inside Gale has ever offered environmental conditions favorable for microbial life, including investigation of the role of water; and planetary habitability studies in preparation for human exploration.
-           </p>
-
-           <div class="slideshow-container">
-             ${buildSlideShow(mars.response.photos)}
-
-             <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-             <a class="next" onclick="plusSlides(1)">&#10095;</a>
-
-          </div>
-          <br>
-
-          <div style="text-align:center">
-            <span class="dot" onclick="currentSlide(1)"></span> 
-            <span class="dot" onclick="currentSlide(2)"></span> 
-            <span class="dot" onclick="currentSlide(3)"></span> 
-          </div>
-
-           <h3>Mission Information: ${mars.response.photos[0].rover.name}</h3>
-           
-          <p> Identifier: ${mars.response.photos[0].rover.id} | Launch Date: ${mars.response.photos[0].rover.launch_date} | Landing Date: ${mars.response.photos[0].rover.landing_date} | Status: ${mars.response.photos[0].rover.status} | Image date: ${mars.response.photos[0].earth_date}</p>
 				</div>
 			</div>
       </section>
    `)
+   } else {
+     return (`<h2>Data Missing</h2>`)
+   }
 }
-
-
-
-
-function buildSlideShow(photos){
-  const Photo_Max = photos.length + 1;
-  let index = 1;
-
-  const slideShow = photos.map((photo) => {
-    return `
-      <div class="mySlides fade">
-         <div class="numbertext">${index=index+1}/${Photo_Max}</div>
-           <img src="${photo.img_src}" data-position="center center" style="width:100%">
-         <div class="text">Camera: ${photo.camera.full_name} | Identifier: ${photo.id} | Image Date: ${photo.earth_date}</div>
-      </div>
-    `;
-  });
-
-  return slideShow.join("");
-}
-
 
 // Call the Curiosity Route - Curiosity Rover NASA API call
-const getImageOfTheDayRover = (state) => {
+const getImageOfTheDayRover = (rover, state) => {
     let { mars } = state
 
-    fetch(`http://localhost:8080/curiosity`)
+    fetch(`http://localhost:8080/${rover.toLowerCase()}`)
         .then(res => res.json())
-        .then(mars => updateStoreRover(store, { mars }))
-}
-// ------------------------------------------------------  COMPONENTS
-
-// ------------------------------------------------------  Slide Show 
-// Ref: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_slideshow
-
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+        .then(mars => updateStoreRover(rover, store, { mars }))
 }
 
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
+// Ref: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_portfolio_gallery
+function buildPortfolioGallery(photos){
+const portfolioImages = photos.map((photo) => {
+  return `
+    <div class="column">
+      <div class="content">
+        <img src="${photo.img_src}" data-position="center center" style="width:100%">
+        <h4>Camera: ${photo.camera.full_name}</h4>
+        <ul>
+           <li>Camera: ${photo.camera.name}</li>
+           <li>Identifier: ${photo.id}</li>
+           <li>Rover Identifier: ${photo.camera.rover_id}</li>
+           <li>Image Date: ${photo.earth_date}</li>
+        </ul>
+    </div>
+  </div>`;
+  });
 
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-    }
-    for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+  return portfolioImages.join("");
 }
-
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -28,6 +28,7 @@ app.get('/apod', async (req, res) => {
 // Rover Curiosity: API call
 app.get('/curiosity', async (req, res) => {
     try {
+        console.log(`curiosity`)
         // Make the API call using the API Key
         let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.API_KEY}`)
             .then(res => { return res.json() })
@@ -41,6 +42,7 @@ app.get('/curiosity', async (req, res) => {
 // Rover Curiosity: API call
 app.get('/opportunity', async (req, res) => {
     try {
+        console.log(`opportunity`)
         // Make the API call using the API Key
         let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/opportunity/photos?sol=1000&api_key=${process.env.API_KEY}`)
             .then(res => { return res.json() })
@@ -54,6 +56,7 @@ app.get('/opportunity', async (req, res) => {
 // Rover Curiosity: API call
 app.get('/spirit', async (req, res) => {
     try {
+        console.log(`spirit`)
         // Make the API call using the API Key
         let response = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=1000&api_key=${process.env.API_KEY}`)
             .then(res => { return res.json() })
